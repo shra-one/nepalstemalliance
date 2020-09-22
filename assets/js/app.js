805163cb8cd875f211c3506628d7368c5bb28a09
Shrawan Shrestha <shrawankumarshrestha@gmail.com>
2020-09-22 19:44:37 +0545

    product detail page completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,11 @@
 
 AOS.init();
+
+$(function(){
+    $('.js-gallary-item').on('click', function(e){
+        var currentSrc = e.currentTarget.currentSrc;
+        var targetDiv = $('.product-gallary__featured');
+        console.log(e)
+        targetDiv.attr('src', currentSrc);
+    })
+})
